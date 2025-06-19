c0476383eeccbffb9cb149c1664fcc30c7759cd9
Rakib Sheikh <mdrakibshak925@gmail.com>
2025-06-19 17:44:58 +0600

    full figma

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Logo from "../assets/Logo.png";
+import Logo from "../assets/13.png";
 import { Menu, X } from "lucide-react"; // ✅ Icon import from lucide-react
 
 const Navbar = () => {
